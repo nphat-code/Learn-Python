07cb6ebdc3ea2bccc362c32032e1e17af0cbfdc5
Nguyen Phat <nguyenphat13112006@gmail.com>
2025-12-22 20:48:14 +0700

    varibles

---
+++ variables.py
@@ -0,0 +1,12 @@
+# Variable = A container for a value (string, integer, float, boolean)
+#            A variable behaves as if it was the value it contains
+
+# Strings
+first_name = "Phat"
+food = "Banh Mi"
+email = "Phat123@gmail.com"
+
+# Integers
+age = 19
+
+print(f"You are {age} years old")
