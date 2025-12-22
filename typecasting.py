60122dbadc1ff5558487341ac4efa22b8c220cc6
Nguyen Phat <nguyenphat13112006@gmail.com>
2025-12-22 20:55:00 +0700

    typecasting

---
+++ typecasting.py
@@ -0,0 +1,9 @@
+# Typecasting = the process of converting a variable from one data type to another
+#               str(), int(), float(), bool()
+name = "Phat Nguyen"
+age = 25
+gpa = 3.2
+is_student = True
+
+name = bool(name)
+print(name)
