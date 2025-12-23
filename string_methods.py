c872aad679671d93a35b765e8a4db6e814f5d0a3
Nguyen Phat <nguyenphat13112006@gmail.com>
2025-12-23 23:31:57 +0700

    string methods

---
+++ string_methods.py
@@ -0,0 +1,15 @@
+# name = input("Enter your full name: ")
+phone_number = input("Enter your phone #: ")
+
+# result = len(name)
+# result = name.find(" ")
+# result = name.rfind(" ")
+# name = name.capitalize()
+# name = name.upper()
+# name = name.lower()
+# result = name.isdigit()
+# result = name.isalpha()
+# result = phone_number.count("-")
+phone_number = phone_number.replace("-", " ")
+
+print(phone_number)
