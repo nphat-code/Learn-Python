8c7c42c897fbd6a573408df5cf47b5ca5d371928
Nguyen Phat <nguyenphat13112006@gmail.com>
2025-12-24 11:26:27 +0700

    2d

---
+++ 2d_collections.py
@@ -0,0 +1,7 @@
+fruits =        ["apple", "orange", "banana", "coconut"]
+vegetables =    ["celery", "carrots", "potatoes"]
+meats =         ["chicken", "fish", "turkey"]
+
+groceries = [fruits, vegetables, meats]
+
+print(groceries[1][1])
